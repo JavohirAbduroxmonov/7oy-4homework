10efc96b2d7bc146588fe02bd59897dc5282582a
Javohir <JavohirAbduroxmonov>
2024-07-29 01:17:39 +0500

    dvsgbxbxb

---
+++ src/App.jsx
@@ -0,0 +1,141 @@
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import NewPost from "./components/NewPost";
+import PostPage from "./components/PostPage";
+import About from "./components/About";
+import Missing from "./components/Missing";
+import { Routes, useNavigate, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
+import axios from "axios";
+
+function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+      editDatetime: "",
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+      editDatetime: "",
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+      editDatetime: "",
+    },
+    {
+      id: 4,
+      title: "My Fourth Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+      editDatetime: "",
+    },
+  ]);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const [fetching, setFetching] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const filteredPosts = posts.filter((post) => {
+      return (
+        post.title.toUpperCase().includes(search.toUpperCase()) ||
+        post.body.toUpperCase().includes(search.toUpperCase())
+      );
+    });
+
+    setSearchResults(filteredPosts);
+  }, [posts, search]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setFetching(true);
+      setError(null);
+      try {
+        const response = await axios.get(`${base_url}/posts`);
+        setPosts(response.data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setFetching(false);
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = {
+      id,
+      title: postTitle,
+      datetime,
+      body: postBody,
+      editDatetime: "",
+    };
+
+    setPosts([...posts, newPost]);
+    setPostTitle("");
+    setPostBody("");
+    navigate("/");
+  };
+
+  const handleDelete = (id) => {
+    const updatedPosts = posts.filter((post) => post.id !== id);
+    setPosts(updatedPosts);
+    navigate("/");
+  };
+
+  return (
+    <div className="App">
+      <Header title="React JS Blog" />
+      <Nav search={search} setSearch={setSearch} />
+      <Routes>
+        <Route path="/" element={<Home posts={searchResults} />} />
+        <Route
+          path="/post"
+          element={
+            <NewPost
+              handleSubmit={handleSubmit}
+              postTitle={postTitle}
+              setPostTitle={setPostTitle}
+              postBody={postBody}
+              setPostBody={setPostBody}
+            />
+          }
+        />
+        <Route
+          path="/post/:id"
+          element={
+            <PostPage
+              handleDelete={handleDelete}
+              posts={posts}
+              setPosts={setPosts}
+            />
+          }
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PostPage.jsx
@@ -0,0 +1,138 @@
+
+import { useEffect, useState } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { format } from "date-fns";
+
+const PostPage = ({ setPosts, posts, handleDelete }) => {
+  const { id } = useParams();
+  const post = posts.find((post) => post.id.toString() === id);
+
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+  const base_url = import.meta.env.VITE_BASE_URL;
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [edit, setEdit] = useState(false);
+
+  const handleEdit = (id) => {
+    const editPosts = posts.map((post) => {
+      if (post.id === id) {
+        return {
+          ...post,
+          title,
+          body,
+          editDatetime: format(new Date(), "MMMM dd, yyyy pp"),
+        };
+      } else {
+        return post;
+      }
+    });
+    setPosts(editPosts);
+    setEdit(false);
+  };
+
+  useEffect(() => {
+    if (post) {
+      setTitle(post.title);
+      setBody(post.body);
+    }
+  }, [post]);
+
+   async function handleDelete(id) {
+   const editedPost = posts.find((post) => post.id === id);
+   setError(null); 
+    try{
+      const response = await fetch(`${base_url}/posts/{id}`,{
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({...editedPost, title, body}),
+      }); 
+       if (!response.ok) {
+        throw new Error("Response was not ok!");
+      }
+    
+         const editedPosts = posts.map((post) => {
+           if (post.id === id) {
+             return { ...post, title, body };
+           } else {
+             return post;
+           }
+         });
+         setPosts(editedPosts);
+         setEdit(false);
+
+    }catch (error){
+        console.error(error);
+        setError(error.message);
+    }
+
+
+ 
+
+  }
+
+  return (
+    <main className="PostPage">
+      {error && <p style={{color: "red"}}>{error}</p>}
+      <article className="post">
+        {post && (
+          <>
+            {edit ? (
+              <input
+                className="title-input"
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            ) : (
+              <h2>{post.title}</h2>
+            )}
+
+            <p className="postDate">{post.datetime}</p>
+            {post.editDatetime && (
+              <p className="editDate">Edited on: {post.editDatetime}</p>
+            )}
+            {edit ? (
+              <textarea
+                className="title-input"
+                rows={5}
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+              ></textarea>
+            ) : (
+              <p className="postBody">{post.body}</p>
+            )}
+            <div className="postButtons">
+              <button onClick={() => handleDelete(post.id)}>Delete Post</button>
+              <button
+                style={{ backgroundColor: edit ? "green" : "blue" }}
+                onClick={() => {
+                  setEdit(!edit);
+                  if (edit) {
+                    handleEdit(post.id);
+                  }
+                }}
+              >
+                {edit ? "Save" : "Edit"}
+              </button>
+            </div>
+          </>
+        )}
+        {!post && (
+          <>
+            <h2>Post Not Found</h2>
+            <p>Well, that's disappointing.</p>
+            <p>
+              <Link to="/">Visit Our Homepage</Link>
+            </p>
+          </>
+        )}
+      </article>
+    </main>
+  );
+};
+
+export default PostPage;
